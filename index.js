ea6a2ac29efe3eb8cc3f2d8604c39a36bcfa8c59
Henna Helin <henna.helin@edu.turkuamk.fi>
2024-06-11 15:27:20 +0300

    3.9

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 
 // middleware morgan 'tiny' + body(POST data), 3.7 & 3.8
